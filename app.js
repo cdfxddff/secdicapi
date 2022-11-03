6c99447662555e49f16730ecd8183154ac23c2ea
yamaguchi <16e4q8yymg@gmail.com>
2022-11-03 21:16:27 +0900

    a

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/',function(req,res){
+    res.send('Working')
+})
+
 app.post('/',async function(req,res){
     const wm = await scr.search(req.body.a)
     res.json(wm)
